0f7582216cd0a70b25e8ee33fbee837486ce72ac
MarcosDasNeves <101129239+MarcosDasNeves@users.noreply.github.com>
2022-03-17 13:57:09 -0300

    Add files via upload

---
+++ Ejercicio7.java
@@ -0,0 +1,33 @@
+package ejercicio.pkg7;
+
+import java.util.Scanner;
+
+
+public class Ejercicio7 {
+
+   
+    public static void main(String[] args) {
+       
+         int a=0,b=13,i=0;
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.print ("Introduzcan un nuemero"); 
+        a=sc.nextInt();
+        
+        
+        if (a<b){
+            System.out.print ("!El numero es MENOR¡"); 
+        } else{
+        
+        }if (a>b){
+            System.out.print ("!El numero es MAYOR¡"); 
+        } else {
+        
+        }if (a==b){
+            System.out.print ("felicitación ganaste"); 
+        }
+      }
+    }
+        
+    
+
